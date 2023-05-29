71d741745db27401e0001c8038dce804b80b838e
Segnicho <segnidesta60@gmail.com>
2023-05-29 19:54:36 +0300

    deployment check

--- slices/apiSlice.ts
+++ slices/apiSlice.ts
@@ -1,11 +1,13 @@
-"use client"
+"use client";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-const API_BASE_URL = "http://localhost:3000/api";
+// const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = "https://meme-share.vercel.app/api";
+
+// https://meme-share.vercel.app/
 export const api = createApi({
   tagTypes: ["Memes", "Users"],
   baseQuery: fetchBaseQuery({
     baseUrl: API_BASE_URL,
   }),
-  endpoints: (builder) =>({})
+  endpoints: (builder) => ({}),
 });
-

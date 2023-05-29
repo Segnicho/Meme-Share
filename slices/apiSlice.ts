0f8ebca7dbbdeeed8b6dd852fd3a3478d913d1c1
Segnicho <segnidesta60@gmail.com>
2023-05-29 21:41:46 +0300

    fix deployment issue

--- slices/apiSlice.ts
+++ slices/apiSlice.ts
@@ -1,6 +1,6 @@
 "use client";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api";
 
 export const api = createApi({
   tagTypes: ["Memes", "Users"],
